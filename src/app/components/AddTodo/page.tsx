c6a0bceca94821016292376ae7bded76af27cc0f
reoroi <reo0330210@gmail.com>
2024-08-14 19:12:17 +0900

    addtodoの追加ボタン完成

--- src/app/components/AddTodo/page.tsx
+++ src/app/components/AddTodo/page.tsx
@@ -1,33 +1,17 @@
-'use client'
+"use client";
 
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import "../../globals.css";
 import Link from "next/link";
+import { handleClientScriptLoad } from "next/script";
+import { handleSubmit } from "@/app/function";
 
 const AddTodo = () => {
-
   const [additionalTodo, setAdditionalTodo] = useState<string>(""); //追加TODO
   const [additionalDate, setAdditionalDate] = useState<string>(""); //追加Date
+  const [additionalTodoDetail, setAdditionalTodoDetail] = useState<string>("");
 
-    // 追加ボタン
-  
-        {/* <div className="input-area">
-            <input
-              onChange={(e) => setAdditionalTodo(e.target.value)}
-              value={additionalTodo}
-              placeholder="TODOを入力してください"
-            />
-            <div className="input-button">
-              <input
-                onChange={(e) => setAdditionalDate(e.target.value)}
-                type="date"
-                value={additionalDate || ""}
-              />
-              <button onClick={clickAdd} className="add-btn">
-                追加
-              </button>
-            </div>
-          </div> */}
+  // 追加ボタン
 
   return (
     <div>
@@ -35,38 +19,54 @@ const AddTodo = () => {
       <div className="addTodo-content">
         <form
           className="addTodo-form"
-          // onSubmit={handleSubmit}
+          onSubmit={async (e) =>
+            await handleSubmit(
+              e,
+              additionalTodo,
+              additionalDate,
+              additionalTodoDetail,
+              setAdditionalDate,
+              setAdditionalTodo,
+              setAdditionalTodoDetail
+            )
+          }
         >
           <div className="content-lastchild">
             <div className="addTodo-content">
-              <p className="addtodo-p">TODO</p>
+              <p className="addtodo-p">TodoTitle</p>
               <input
                 className="addTodo-input"
-                placeholder="Todoを入力してください"
-                // onChange={(e) => setId(e.target.value)}
+                placeholder="TodoTitleを入力してください"
+                value={additionalTodo}
+                onChange={(e:ChangeEvent<HTMLInputElement>) => setAdditionalTodo(e.target.value)}
                 type="text"
+              />
+            </div>
+            <div className="addTodo-content">
+              <p className="addtodo-p">TODO詳細内容</p>
+              <textarea
+                className="addTodo-input"
+                placeholder="Todoの詳細内容を入力してください"
+                onChange={(e:ChangeEvent<HTMLTextAreaElement>) => setAdditionalTodoDetail(e.target.value)}
+                rows={5}
+                value={additionalTodoDetail}
               />
             </div>
             <div className="addTodo-content">
               <p className="addtodo-p">期限日付</p>
               <input
-                // onChange={(e) => setTitle(e.target.value)}
+                onChange={(e) => setAdditionalDate(e.target.value)}
                 className="addTodo-input"
                 type="date"
+                value={additionalDate}
               />
             </div>
           </div>
           <div className="addTodo-Horizontal">
-          <button
-          className=""
-            type="submit"
-            // disabled={loading}
-          >
-            追加
-          </button>
-          <Link href={'/'}>
-          <button>戻る</button>
-          </Link>
+            <button type="submit">追加</button>
+            <Link href={"/"}>
+              <button>戻る</button>
+            </Link>
           </div>
         </form>
       </div>

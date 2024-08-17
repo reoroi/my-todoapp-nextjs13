e856857f1c02e3f0fd23b672d7dd2fabf7754b34
reoroi <reo0330210@gmail.com>
2024-08-17 19:07:13 +0900

    firebaseからtodoDetailも取得できるよう追加

--- src/app/GetTodoData.tsx
+++ src/app/GetTodoData.tsx
@@ -13,11 +13,13 @@ export const useGetTodoList = () => {
     const unsubscribe = onSnapshot(todoData, (querySnapshot) => {
       const todos = querySnapshot.docs.map((doc) => {
         const data = doc.data();
+
         return {
           id: doc.id,
           todo: data.todo,
           deadLineDate: data.deadLineDate,
           status: data.status,
+          todoDetail:data.todoDetail
         } as TodoItemType;
       });
       setTodoList(todos); // 取得したデータを状態に保存
@@ -28,3 +30,5 @@ export const useGetTodoList = () => {
   }, []);
   return todoList
 }
+
+

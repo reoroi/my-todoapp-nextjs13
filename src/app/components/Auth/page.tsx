1a2bbf7a6656a419ea1e62b30b77d62eaf58ef98
reoroi <reo0330210@gmail.com>
2024-09-26 15:24:36 +0900

    vscodeでのプッシュテスト

--- src/app/components/Auth/page.tsx
+++ src/app/components/Auth/page.tsx
@@ -24,6 +24,7 @@ const AddNewUser = () => {
     <div className="loginFormContent">
       <Link href={"/"}>
         <button>戻る</button>
+        
       </Link>
       <div className="formContent">
         <form onSubmit={handleSubmit((loginData)=>registrationButton(loginData,setAuthError,router))} className="formContent">
